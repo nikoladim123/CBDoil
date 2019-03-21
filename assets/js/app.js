08d1d011233866470617f4c9137a3f3cb979ad18
unknown <nikoladimitrijevic1991@gmail.com>
2019-03-21 18:14:28 +0100

    weed

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,27 @@
 //   }
 // }
 //
+
+
+// baner bottles animation
+// baner bottles animation
+// baner bottles animation
+var oilBottles = document.getElementsByClassName("oilBottles")[0];
+var bottleShadow = document.getElementsByClassName("bottleShadow")[0];
+function oilBotFun(){
+  oilBottles.style.left = '8.2vw'
+  bottleShadow.style.left = '8.2vw'
+  oilBottles.style.opacity = '0.5';
+  bottleShadow.style.opacity = '0.5';
+  setTimeout(function () {
+    oilBottles.style.opacity = '1';
+    bottleShadow.style.opacity = '1';
+  }, 1500);
+}
+
+window.onload = function(){
+  oilBotFun();
+}
 // window.onscroll = function(){
 //   mobVidCall();
 // }
