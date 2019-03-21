866eade81af8e035efa5f267fc1b6d96ce45cef2
unknown <nikoladimitrijevic1991@gmail.com>
2019-03-21 16:12:19 +0100

    weed

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// drop paralax
+var oilDrop = document.getElementsByClassName("oilDrop")[0];
+function mobVidCall(){
+	if (parseInt(window.getComputedStyle(oilDrop).bottom) > -(window.innerWidth/100 *36) ) {
+    oilDrop.style.bottom = parseInt(window.getComputedStyle(oilDrop).bottom) - window.scrollY/130 + 'px';
+  }
+}
+
+window.onscroll = function(){
+  mobVidCall();
+}
